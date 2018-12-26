be6949abc23d50d5f134fc1cb96cdfafbe60ed7e
Atsushi Kokubu <akokubu@longposition.co.jp>
2018-12-26 09:37:21 +0900

    チャンネル情報取得

--- src/renderer/components/Channel.vue
+++ src/renderer/components/Channel.vue
@@ -1,11 +1,55 @@
 <template>
   <div>
-    Channel {{ $route.params.id }}
+    <p><img :src='channel.thumbnail' class="image-round"/>{{ channel.title }}</p>
     <router-link :to="{ name: 'home' }"> back </router-link>
   </div>
 </template>
 
 <script>
+const {google} = require('googleapis')
+const youtube = google.youtube({
+  version: 'v3',
+  auth: localStorage.getItem('authKey')
+})
 
+export default {
+  data () {
+    return {
+      channel: {
+        id: '',
+        title: '',
+        thumbnail: ''
+      }
+    }
+  },
+  methods: {
+    getChannel (channelId) {
+      const params = {
+        part: 'snippet',
+        id: channelId
+      }
 
+      youtube.channels.list(params, (err, data) => {
+        if (err) {
+          console.log(err)
+        }
+        var ch = data.data.items[0].snippet
+        this.channel = {
+          id: params.id,
+          title: ch.title,
+          thumbnail: ch.thumbnails.medium.url
+        }
+      })
+    }
+  },
+  mounted: function () {
+    this.getChannel(this.$route.params.id)
+  }
+}
 </script>
+
+<style>
+.image-round {
+  border-radius: 50%;
+}
+</style>
